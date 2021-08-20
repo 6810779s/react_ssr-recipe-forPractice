940ecb97acef9d6c7b842ddde2ce102e6fae69b5
EUNHEE JANG <6810779s@naver.com>
2021-08-21 00:02:05 +0900

    add

---
+++ scripts/build.server.js
@@ -0,0 +1,29 @@
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
+process.on('unhandledRejection', (err) => {
+  throw err;
+});
+
+require('../config/env');
+const fs = require('fs-extra');
+const webpack = require('../config/webpack.config.server');
+const config = require('../config/webpack.config.server');
+const paths = require('../config/paths');
+
+function build() {
+  console.log('creating server build...');
+  fs.emptyDirSync(paths.ssrBuild);
+  let compiler = webpack(config);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, state) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(state.toString());
+    });
+  });
+}
+
+build();
